3770b9911ced590bdd7aff47c66d80d9927e994a
Waliullah Muntasir <waliullah.muntasir@gmail.com>
2025-01-04 10:23:27 +0600

    First release of Hello World!

---
+++ js.js
@@ -0,0 +1,15 @@
+
+// function celToFahreit(suii) {
+//     return (suii * 9/5) + 32;
+// }
+
+// console.log(celToFahreit(25));
+
+var hights = [10,20,30,444444, 609,69];
+var max = hights[0];
+for(var i = 0; i < hights.length; i++){
+    if(max< hights[i]){
+        max = hights[i];
+    }
+}
+console.log(max)
