a50b30216f4596a572542a1daba40c798e672913
Waliullah Muntasir <waliullah.muntasir@gmail.com>
2025-01-09 15:43:00 +0600

    ur

--- js.js
+++ js.js
@@ -5,11 +5,46 @@
 
 // console.log(celToFahreit(25));
 
-var hights = [10,20,30,444444, 609,69];
-var max = hights[0];
-for(var i = 0; i < hights.length; i++){
-    if(max< hights[i]){
-        max = hights[i];
-    }
-}
-console.log(max)
+// var hights = [10,20,30,444444, 609,69];
+// var max = hights[0];
+// for(var i = 0; i < hights.length; i++){
+//     if(max< hights[i]){
+//         max = hights[i];
+//     }
+// }
+// console.log(max)
+
+
+// function calculatemoney(number){
+//     var ticketmoney = number * 120;
+//     var spentone = 8 * 50;
+//     var spenttwo = spentone + 500;
+//     var minus = ticketmoney - spenttwo;
+//     console.log(minus)
+// }
+// calculatemoney(1);
+
+// function checkName(name) {
+  
+//     let characters = name.split('');
+
+  
+//     let lastChar = characters[characters.length - 1].toLowerCase();
+
+
+//     const allowedChars = ['a', 'y', 'i', 'e', 'o', 'u', 'w'];
+
+   
+//     if (allowedChars.includes(lastChar)) {
+//         return 'godd';
+//     } else {
+//         return 'bad';
+//     }
+// }
+
+// // Example usage
+// console.log(checkName('Mary'));
+// console.log(checkName('John'));
+// console.log(checkName('Eve')); 
+
+
